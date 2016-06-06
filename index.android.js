9de57d6166a4ac533463b3605ddc460bc8c6349a
Matt MacAulay <mac@twg.ca>
2016-06-05 20:04:35 -0400

    standard

--- index.android.js
+++ index.android.js
@@ -3,29 +3,24 @@
  * https://github.com/facebook/react-native
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View } from 'react-native'
 
 class GoTransitMobile extends Component {
-  render() {
+  render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to React Native!
+      </Text>
+      <Text style={styles.instructions}>
+        To get started, edit index.android.js
+      </Text>
+      <Text style={styles.instructions}>
+        Shake or press menu button for dev menu
+      </Text>
+    </View>
+    )
   }
 }
 
@@ -34,18 +29,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
 
-AppRegistry.registerComponent('GoTransitMobile', () => GoTransitMobile);
+AppRegistry.registerComponent('GoTransitMobile', () => GoTransitMobile)
